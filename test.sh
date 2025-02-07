935b959f27576cd05f3fffa149f060510d8e1636
ekahPruthvi <p1sribhat@gmail.com>
2025-02-07 10:56:53 +0530

    copy function

--- test.sh
+++ test.sh
@@ -1,19 +1,36 @@
 #! /bin/bash
 
 test(){
+    local flag=1
+    eval last=\${$#}
+    if [ -f $last ]; then
+        echo "penguin asks if you want to rewrite (Y/n)"
+        read choice
+        if [ "$choice" = "y" ] || [ "$choice" = "Y" ] || [ "$choice" = "" ]; then
+            echo "rewriting $last"
+            flag=1
+        else
+            flag=0
+            return 
+        fi
+    else 
+        flag=1
+    fi
     if [ $# -lt 2 ]; then
-        echo "penguin asks where to move to?"
-        exit
+        echo "penguin says invalid input," 
+        echo "give atleast souce file/directory and destination file/directory"
     else
         for i in $@; do
-            if [ -e $i ]; then
+            if [ -e $i ] || [ "$i" = "$last" ]; then
                 continue
             else
                 echo "penguin don't think it exits : $i "
                 break
             fi
         done
-        mv $@ 2> /dev/null
+        if [ "$flag" -eq 1 ]; then
+            cp $@ 2> /dev/null
+        fi
     fi
 }
 
