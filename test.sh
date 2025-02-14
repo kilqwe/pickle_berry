3f12dc872444c1521eb24e4edfeb10f9c128facd
ekahPruthvi <p1sribhat@gmail.com>
2025-02-14 21:41:28 +0530

    testing delete

--- test.sh
+++ test.sh
@@ -3,12 +3,15 @@
 test(){
     local inp
     if [ $# -ne 1 ]; then
-        echo "penguin can not delete on mutiple files/folders" && sleep 2s
-    elif [ -f $1 ]; then
-        printf " DELETE $1 ? (y/n)"
+        echo "penguin can not delete mutiple files/folders" && sleep 2s
+    elif [ "$1" = "/" ] || [ "$1" = "//" ] ; then
+        echo "penguin will not perform this action" && sleep 2s
+    elif [ -e $1 ]; then
+        #tput cup $(( $ll - 2 )) 3
+        printf " DELETE $1? (y/n) : "
         read inp
-        if [ "$inp" = "y" ]; then
-            echo
+        if [ "$inp" = "y" ] && [ -f $1 ]; then
+            mv $1 ~/.local/share/Trash/files 2> /dev/null
         fi
     else
         echo "penguin don't think $1 exits" && sleep 2s
