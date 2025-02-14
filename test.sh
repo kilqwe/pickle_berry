f493a51e4fa3b6f46176841f0c44d35ce513ab6b
ekahPruthvi <p1sribhat@gmail.com>
2025-02-14 21:16:30 +0530

    testing delete

--- test.sh
+++ test.sh
@@ -1,36 +1,17 @@
 #! /bin/bash
 
 test(){
-    local flag=1
-    eval last=\${$#}
-    if [ -f $last ]; then
-        echo "penguin asks if you want to rewrite (Y/n)"
-        read choice
-        if [ "$choice" = "y" ] || [ "$choice" = "Y" ] || [ "$choice" = "" ]; then
-            echo "rewriting $last"
-            flag=1
-        else
-            flag=0
-            return 
+    local inp
+    if [ $# -ne 1 ]; then
+        echo "penguin can not delete on mutiple files/folders" && sleep 2s
+    elif [ -f $1 ]; then
+        printf " DELETE $1 ? (y/n)"
+        read inp
+        if [ "$inp" = "y" ]; then
+            echo
         fi
-    else 
-        flag=1
-    fi
-    if [ $# -lt 2 ]; then
-        echo "penguin says invalid input," 
-        echo "give atleast souce file/directory and destination file/directory"
     else
-        for i in $@; do
-            if [ -e $i ] || [ "$i" = "$last" ]; then
-                continue
-            else
-                echo "penguin don't think $i exits"
-                break
-            fi
-        done
-        if [ "$flag" -eq 1 ]; then
-            cp $@ 2> /dev/null
-        fi
+        echo "penguin don't think $1 exits" && sleep 2s
     fi
 }
 
