f8bb85fa12b2ff55a30150479d11de800a879c2b
ekahPruthvi <p1sribhat@gmail.com>
2025-02-18 07:14:50 +0530

    testing -open

--- test.sh
+++ test.sh
@@ -2,14 +2,14 @@
 
 test(){
     local inp
-    if [ $# -ne 1 ]; then
-        echo "penguin can not delete mutiple files/folders" && sleep 2s
-    elif [ "$1" = "/" ] || [ "$1" = "//" ] ; then
-        echo "penguin will not perform this action" && sleep 2s
+    if [ $# -gt 1 ]; then
+        echo "penguin can not open mutiple files/folders" && sleep 2s
+    elif [ $# -lt 1 ]; then
+        echo "penguin has to open somthing ??" && sleep 2s
     elif [ -e $1 ]; then
         #make_window
         #tput cup $(( $ll - 2 )) 3
-        code $1 && ./Documents/grime/test.sh
+        vim $1 && ./Documents/grime/test.sh
     else
         echo "penguin don't think $1 exits" && sleep 2s
     fi
